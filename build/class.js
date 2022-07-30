1ff7da5006d64c89025b3dd389aa9ba3c6dd9d0d
Jeongmmin <seo88kr@hanmail.net>
2022-07-30 23:59:44 +0900

    Add class.ts

---
+++ build/class.js
@@ -0,0 +1,47 @@
+// 단어사전 만들기
+class Dict {
+    constructor() {
+        this.words = {};
+    }
+    add(word) {
+        if (this.words[word.term] === undefined) {
+            this.words[word.term] = word.def;
+        }
+    }
+    get(term) {
+        return this.words[term];
+    }
+    delete(term) {
+        return delete this.words[term];
+    }
+    update(term, newDef) {
+        this.words[term] = newDef;
+        return this.words[term];
+    }
+    showAll() {
+        for (var key in this.words) {
+            console.log(`${key} : ${this.words[key]}`);
+        }
+    }
+    count() {
+        return Object.keys(this.words).length;
+    }
+}
+class Word {
+    constructor(term, def) {
+        this.term = term;
+        this.def = def;
+    }
+}
+const abstract_mean = new Word('abstract', '추상적');
+const implements_mean = new Word('implements', '구현');
+const method = new Word('method', 'class 안의 함수를 의미함');
+const dict = new Dict();
+dict.add(abstract_mean);
+dict.add(implements_mean);
+dict.add(method);
+dict.get('abstract');
+dict.delete('implements');
+dict.update('abstract', '추상적인 이라는 의미');
+dict.showAll();
+console.log(dict.count());
